bbbb3d1fd0a43dbed685408e4083b13404b137c2
satya2215101040 <133234144+satya2215101040@users.noreply.github.com>
2023-07-12 17:48:10 +0800

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+int main() {
+    std::queue<int> myQueue;
+
+    
+    myQueue.push(10);
+    myQueue.push(20);
+    myQueue.push(30);
+
+    
+    std::cout << "Elemen pada depan antrian: " << myQueue.front() << std::endl;
+
+ 
+    std::cout << "Elemen pada belakang antrian: " << myQueue.back() << std::endl;
+
+    
+    myQueue.pop();
+
+    
+    std::cout << "Isi antrian setelah pop: ";
+    while (!myQueue.empty()) {
+        std::cout << myQueue.front() << " ";
+        myQueue.pop();
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
